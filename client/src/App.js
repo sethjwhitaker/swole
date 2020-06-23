a987d90be45bdbe2963d97bc451fa02957040649
Seth Whitaker <sethjwhitaker@yahoo.com>
2020-06-22 22:03:29 -0700

    Initial React files

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          This is the Swole Fitness-Tracker App
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
